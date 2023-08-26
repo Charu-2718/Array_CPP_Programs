c5383dd0b785d2e18631b6f9f3cc288d4044a14b
Charu-2718 <charu.15421@gmail.com>
2023-08-26 21:48:21 +0530

    Sorting

---
+++ Sort Array/bubblesort.cpp
@@ -0,0 +1,47 @@
+
+#include <iostream>
+using namespace std;
+
+void createArray(int arr[],int n){
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+   
+}
+
+void bubbleSort(int arr[], int n)
+{
+	int i, j;
+	bool swapped;
+	for (i = 0; i < n - 1; i++) {
+		swapped = false;
+		for (j = 0; j < n - i - 1; j++) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+				swapped = true;
+			}
+		}
+
+		if (swapped == false)
+			break;
+	}
+}
+
+void printArray(int arr[], int size)
+{
+	int i;
+	for (i = 0; i < size; i++)
+		cout << " " << arr[i];
+}
+
+int main()
+{
+	int N;
+    cin>>N;
+    int arr[N];
+    createArray(arr,N);
+	bubbleSort(arr, N);
+	cout << "Sorted array: \n";
+	printArray(arr, N);
+	return 0;
+}
