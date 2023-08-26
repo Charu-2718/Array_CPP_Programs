5307940d61f2c50c7e4d1e050f8d958307421357
Charu-2718 <charu.15421@gmail.com>
2023-08-27 03:46:47 +0530

    modified:   .vscode/settings.json 	new file:   Sort Array/EasyProblems.cpp 	new file:   Sort Array/Heap.cpp 	new file:   Sort Array/MediumProblems.cpp 	new file:   Sort Array/StrandSort.cpp 	new file:   Sort Array/bucketsort.cpp 	new file:   Sort Array/cocktailSort.cpp 	new file:   Sort Array/combsort.cpp 	new file:   Sort Array/countingsort.cpp 	new file:   Sort Array/cyclesort.cpp 	new file:   Sort Array/gnomesort.cpp 	new file:   Sort Array/radixsort.cpp 	new file:   Sort Array/shellsort.cpp 	new file:   Sort Array/timsort.cpp 	new file:   Sort Array/treesort.cpp

---
+++ Sort Array/EasyProblems.cpp
@@ -0,0 +1,13 @@
+// 1.)Sort elements by frequency
+// 2.)Sort an array of 0s, 1s and 2s
+// 3.)Sort numbers stored on different machines
+// 4.)Sort an array in wave form
+// 5.)Check if any two intervals overlap among a given set of intervals
+// 6.)How to sort an array of dates in C/C++?
+// 7.)Sorting Strings using Bubble Sort
+// 8.)Find missing elements of a range
+// 9.)Sort an array according to count of set bits
+// 10.)Sort even-placed elements in increasing and odd-placed in decreasing order
+// 11.)Sort an array when two halves are sorted
+// 12.)Sorting Big Integers
+// 13.)Sort a linked list of 0s, 1s and 2s
---
+++ Sort Array/MediumProblems.cpp
@@ -0,0 +1,16 @@
+// 1.)Inversion count in Array using Merge Sort
+// 2.)Find the Minimum length Unsorted Subarray, sorting which makes the complete array sorted
+// 3.)Sort a nearly sorted (or K sorted) array
+// 4.)Sort n numbers in range from 0 to n^2 – 1 in linear time
+// 5.)Sort an array according to the order defined by another array
+// 6.)Find the point where maximum intervals overlap
+// 7.)Find a permutation that causes worst case of Merge Sort
+// 8.)Sort Vector of Pairs in ascending order in C++
+// 9.)Minimum swaps to make two arrays identical
+// 10.)Chocolate Distribution Problem
+// 11.)Permute two arrays such that sum of every pair is greater or equal to K
+// 12.)Bucket Sort To Sort an Array with Negative Numbers
+// 13.)Sort a Matrix in all way increasing order
+// 14.)Convert an Array to reduced form using Vector of pairs
+// 15.)Smallest Difference Triplet from Three arrays
+// 16.)Check if it is possible to sort an array with conditional swapping of adjacent allowed
