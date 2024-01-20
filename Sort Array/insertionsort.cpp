7af585e4fb6bdf39902d716d6b7d0894c514fbdb
Charu Gupta <charu.15421@gmail.com>
2024-01-21 00:47:24 +0530

    Update insertionsort.cpp

--- Sort Array/insertionsort.cpp
+++ Sort Array/insertionsort.cpp
@@ -1,51 +1,32 @@
-
 #include <iostream>
 using namespace std;
 
-void createArray(int arr[],int n){
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
+// Function to print an array
+void printArray(int array[], int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+
+void insertionSort(int array[], int size) {
+  for (int step = 1; step < size; step++) {
+    int key = array[step];
+    int j = step - 1;
+
+    while (key < array[j] && j >= 0) {
+      array[j + 1] = array[j];
+      --j;
     }
-   
+    array[j + 1] = key;
+  }
 }
 
-void insertionSort(int arr[], int n)
-{
-    int key;
-    for (int i = 1; i < n; i++) {
-        key = arr[i];
-        
-
-        for(int j = i-1 ;j >= 0;  j--) {
-            if (arr[j] > key){
-                arr[j + 1] = arr[j];
-
-            }
-            else{
-                break;
-            }
-            
-           
-        }
-        arr[j + 1] = key;
-    }
-}
-
-void printArray(int arr[], int size)
-{
-	int i;
-	for (i = 0; i < size; i++)
-		cout << " " << arr[i];
-}
-
-int main()
-{
-	int N;
-    cin>>N;
-    int arr[N];
-    createArray(arr,N);
-	insertionSort(arr, N);
-	cout << "Sorted array: \n";
-	printArray(arr, N);
-	return 0;
+// Driver code
+int main() {
+  int data[] = {9, 5, 1, 4, 3};
+  int size = sizeof(data) / sizeof(data[0]);
+  insertionSort(data, size);
+  cout << "Sorted array in ascending order:\n";
+  printArray(data, size);
 }
